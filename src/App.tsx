1496c2d89a410e10826c1c89cd3e37a63f3ac544
lukashetzendorfer-boop <lukas.hetzendorfer@gmail.com>
2026-03-10 13:33:52 +0100

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Leistungen from './components/Leistungen'
+import UeberUns from './components/UeberUns'
+import Statistiken from './components/Statistiken'
+import Presse from './components/Presse'
+import Kontakt from './components/Kontakt'
+import Footer from './components/Footer'
+import NewsletterModal from './components/NewsletterModal'
+
+export default function App() {
+  const [modalOpen, setModalOpen] = useState(false)
+
+  return (
+    <div className="min-h-screen">
+      <Navbar onNewsletterClick={() => setModalOpen(true)} />
+      <main>
+        <Hero onNewsletterClick={() => setModalOpen(true)} />
+        <Leistungen />
+        <UeberUns />
+        <Statistiken />
+        <Presse />
+        <Kontakt />
+      </main>
+      <Footer onNewsletterClick={() => setModalOpen(true)} />
+      <NewsletterModal isOpen={modalOpen} onClose={() => setModalOpen(false)} />
+    </div>
+  )
+}
